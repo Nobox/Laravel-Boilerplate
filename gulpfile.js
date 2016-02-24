6aa02ec07e1a06931133479b856b776ba7261a17
Waldemar Figueroa <waldemar@nobox.com>
2016-02-24 12:43:29 -0500

    Update gulpfile JS to ES6 with linter compliance

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,5 @@
-var elixir = require('laravel-elixir');
+const elixir = require('laravel-elixir');
+const config = elixir.config;
 require('laravel-elixir-modernizr');
 require('./elixir-extensions/imagemin');
 require('./elixir-extensions/svg-sprite');
@@ -6,50 +7,51 @@ require('./elixir-extensions/spritesmith');
 
 // Set SASS options
 config.css.sass.pluginOptions = {
-    includePaths: [
-        './node_modules',
-        config.publicPath + '/bower_components'
-    ],
+  includePaths: [
+    './node_modules',
+    `${config.publicPath}/bower_components`,
+  ],
 };
 
 // Add IE9 support to autoprefixer options.
 config.css.autoprefix.options = {
-    browsers: ['ie >= 9', 'last 2 versions'],
-    cascade: false
+  browsers: ['ie >= 9', 'last 2 versions'],
+  cascade: false,
 };
 
 // Enable watchify polling for our NFS-mounted VMs.
 config.js.browserify.watchify.options.poll = true;
 
 // Mix it
-elixir(function(mix) {
-    mix
-        // Optimize project images
-        .imagemin()
+elixir((mix) => {
+  mix
+    // Optimize project images
+    .imagemin()
 
-        // Create optimized SVG spritesheet
-        .svgSprite()
+    // Create optimized SVG spritesheet
+    .svgSprite()
 
-        // Create optimized SVG spritesheet
-        .spritesmith()
+    // Create optimized SVG spritesheet
+    .spritesmith()
 
-        // Compile main SASS stylesheet
-        .sass('styles.scss')
+    // Compile main SASS stylesheet
+    .sass('styles.scss')
 
-        // Pass JS code through Browserify and create bundle
-        .browserify('entry.js', config.get('public.js.outputFolder') + '/bundle.js')
+    // Pass JS code through Browserify and create bundle
+    .browserify('entry.js', `${config.get('public.js.outputFolder')}/bundle.js`)
 
-        // Custom modernizr build
-        .modernizr(null, null, {
-            excludeTests: ['hidden'],
-            options: ['setClasses']
-        })
+    // Custom modernizr build
+    .modernizr(null, null, {
+      excludeTests: ['hidden'],
+      options: ['setClasses'],
+    })
 
-        // Cache-busting like a boss
-        .version([
-            'js/bundle.js',
-            'css/styles.css',
-            'svg/sprite.svg',
-            'js/vendor/modernizr-custom.js'
-        ])
+    // Cache-busting like a boss
+    .version([
+      'js/bundle.js',
+      'css/styles.css',
+      'svg/sprite.svg',
+      'js/vendor/modernizr-custom.js',
+    ])
+  ;
 });
