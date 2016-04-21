d3c902d6257d25c74322d5679b3c27eae94b3618
Waldemar Figueroa <waldemar@nobox.com>
2016-04-21 16:22:58 -0400

    Update jQuery version to 2.2.3

--- resources/views/layouts/site.blade.php
+++ resources/views/layouts/site.blade.php
@@ -39,8 +39,8 @@
     @endif
 
     {{-- jQuery --}}
-    <script src="https://code.jquery.com/jquery-2.2.0.min.js"></script>
-    <script>window.jQuery || document.write('<script src="bower_components/jquery/dist/jquery.min.js"><\/script>')</script>
+    <script src="https://code.jquery.com/jquery-2.2.3.min.js"></script>
+    <script>window.jQuery || document.write('<script src="/bower_components/jquery/dist/jquery.min.js"><\/script>')</script>
 @stop
 
 @section('scripts-site')
