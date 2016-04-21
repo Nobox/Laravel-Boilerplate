d925ac13a81155ccdd140c66ba8a71d84ebf782a
Waldemar Figueroa <waldemar@nobox.com>
2016-04-21 16:34:25 -0400

    Add google analytics layout section.
    Tracking ID comes from environment variable set in .env file

--- resources/views/layouts/base.blade.php
+++ resources/views/layouts/base.blade.php
@@ -39,6 +39,24 @@
 
         @yield('body')
 
+        @section('analytics')
+            {{-- Google Analytics --}}
+            <script>
+                (function(b,o,i,l,e,r){b.GoogleAnalyticsObject=l;b[l]||(b[l]=
+                function(){(b[l].q=b[l].q||[]).push(arguments)});b[l].l=+new Date;
+                e=o.createElement(i);r=o.getElementsByTagName(i)[0];
+                e.src='https://www.google-analytics.com/analytics.js';
+                r.parentNode.insertBefore(e,r)}(window,document,'script','ga'));
+
+                ga('create', '{{ env("GA_TRACKING_ID", "UA-XXXXX-X") }}', 'auto');
+                ga('send', 'pageview');
+
+                @if (empty(env('GA_TRACKING_ID')) || env('GA_TRACKING_ID') == 'UA-XXXXX-X')
+                    console.warn('Warning: Remember to set a valid Google Analytics property tracking ID.');
+                @endif
+            </script>
+        @show
+
         @section('scripts-plugins')
             {{-- Plugin scripts --}}
         @show
