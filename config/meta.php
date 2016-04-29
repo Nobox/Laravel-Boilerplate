2f2c1719e8bcd70f9ae10e0a3cf6a25b8151d1f4
Waldemar Figueroa <waldemar@nobox.com>
2016-04-28 21:27:28 -0400

    Add app meta config values
    Values for random app meta like name, client, and other data for consistent reuse and easy access.

---
+++ config/meta.php
@@ -0,0 +1,28 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Application Meta
+    |--------------------------------------------------------------------------
+    |
+    | These are custom meta data values for easy app-wide use.
+    |
+    */
+
+    'app' => [
+        'name' => 'Laravel Boilerplate',
+        'client' => 'Nobox',
+        'description' => 'Better marketing is social at the core!™',
+    ],
+
+    /*
+     * App's theme and branding customization
+     */
+    'brand' => [
+        'status-bar-color' => '#aa4ccd', // Mobile browser status bar background color
+        'tile-color' => '#30343b', // Windows tile background color
+    ],
+
+];
