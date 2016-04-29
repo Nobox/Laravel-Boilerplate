c87eb44e81af13bd664e8c954d84f13f251336b0
Waldemar Figueroa <waldemar@nobox.com>
2016-04-28 21:40:19 -0400

    Enhance mobile web app support and customization
    - Add most important touch icons and images
    - Customize 'Add to homescreen' for Chrome on Android
    - Customize 'Add to homescreen' for Safari on iOS
    - Add styles to Windows tiles and mobile status bars
    
    All icons, images and colors are FPO. Must be customized for each app and client.

--- resources/views/layouts/site.blade.php
+++ resources/views/layouts/site.blade.php
@@ -1,5 +1,27 @@
 @extends('layouts.base')
 
+@section('meta')
+    @parent
+
+    {{-- Add to homescreen for Chrome on Android --}}
+    <meta name="mobile-web-app-capable" content="yes">
+    <meta name="application-name" content="{{ config('meta.app.name') }}">
+    <link rel="icon" sizes="192x192" href="/img/touch/chrome-touch-icon-192x192.png">
+
+    {{-- Add to homescreen for Safari on iOS --}}
+    <meta name="apple-mobile-web-app-capable" content="yes">
+    <meta name="apple-mobile-web-app-status-bar-style" content="{{ config('meta.brand.status-bar-color') }}">
+    <meta name="apple-mobile-web-app-title" content="{{ config('meta.app.name') }}">
+    <link rel="apple-touch-icon" href="/img/touch/apple-touch-icon.png">
+
+    {{-- Tile icon for Win8 (144x144 + tile color) --}}
+    <meta name="msapplication-TileImage" content="/img/touch/ms-touch-icon-144x144-precomposed.png">
+    <meta name="msapplication-TileColor" content="{{ config('meta.brand.tile-color') }}">
+
+    {{-- Color the status bar on mobile devices --}}
+    <meta name="theme-color" content="{{ config('meta.brand.status-bar-color') }}">
+@stop
+
 @section('prefetch')
   <link rel="dns-prefetch" href="https://code.jquery.com">
 @stop
